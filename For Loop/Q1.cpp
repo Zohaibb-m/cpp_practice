db661fae999c7f9552cb1f8f70419841c33260c5
Muhammad Samee <muhammad_samee23@yahoo.com>
2023-06-16 20:23:38 +0500

    - For loop

---
+++ For Loop/Q1.cpp
@@ -0,0 +1,12 @@
+//Write a program in C++ to find the first 10 natural numbers.
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    //This loop runs 10 times and prints the value of i everytime
+    for (int i=0;i<10;i++){
+        cout << i << endl;
+    }
+}
