c0560f4c5df15250211e6f01f8a5d018dbee2d34
Zohaib <zohaibmunir32@gmail.com>
2023-07-23 16:18:33 +0500

    Grand Test Added

---
+++ Grand_Test_1/Q7.cpp
@@ -0,0 +1,10 @@
+/*
+    Write a function that takes two values x,y as parameters and returns
+    an integer value. The integer is the quadrant in which the point(x, y) lies.
+    Take x and y as input from the user and print the quadrant in which the point lies.
+    Sample Input:
+    Enter x: 5
+    Enter y: 6
+    Sample Output:
+    Quadrant 1
+*/
